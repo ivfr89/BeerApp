4025c590649627547b36d7e46e75e226dec39bad
ivan <ivanfr89@gmail.com>
2022-08-28 13:51:12 +0200

    Removed unused files

--- app/src/main/java/com/developer/ivan/beerapp/di/Annotations.kt
+++
@@ -1,9 +0,0 @@
-package com.developer.ivan.beerapp.di
-
-import javax.inject.Scope
-
-
-@Scope
-@MustBeDocumented
-@Retention(value = AnnotationRetention.RUNTIME)
-annotation class PerFragment
--- app/src/main/java/com/developer/ivan/beerapp/di/AppSubcomponents.kt
+++
@@ -1,8 +0,0 @@
-package com.developer.ivan.beerapp.di
-
-import com.developer.ivan.beerapp.ui.main.di.BeerDetailSubComponent
-import com.developer.ivan.beerapp.ui.main.di.BeerListSubComponent
-import dagger.Module
-
-@Module(subcomponents = [BeerListSubComponent::class, BeerDetailSubComponent::class])
-interface AppSubcomponents
