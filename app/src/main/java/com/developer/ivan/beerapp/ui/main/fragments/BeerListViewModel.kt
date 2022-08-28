13f99dfee77d1d70761ee95d4287ca1922ca4002
ivan <ivanfr89@gmail.com>
2022-08-28 13:54:57 +0200

    BeerListViewModel refactor with stateflow and extensions

--- app/src/main/java/com/developer/ivan/beerapp/ui/main/fragments/BeerListViewModel.kt
+++ app/src/main/java/com/developer/ivan/beerapp/ui/main/fragments/BeerListViewModel.kt
@@ -1,87 +1,66 @@
 package com.developer.ivan.beerapp.ui.main.fragments
 
-import android.os.Parcelable
-import android.widget.TextView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.developer.ivan.beerapp.core.Event
-import com.developer.ivan.beerapp.di.PerFragment
-import com.developer.ivan.beerapp.ui.main.models.UIBeer
-import com.developer.ivan.beerapp.ui.mapper.UIMapper
+import com.developer.ivan.beerapp.ui.main.BeerListState
+import com.developer.ivan.beerapp.ui.mapper.toUi
+import com.developer.ivan.beerapp.ui.utils.CoroutineManageContext
+import com.developer.ivan.beerapp.ui.utils.CoroutineUtils
 import com.developer.ivan.domain.Failure
-import com.developer.ivan.usecases.GetBeers
+import com.developer.ivan.interactors.GetBeers
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Suppress("RedundantSuspendModifier")
-@PerFragment
+@HiltViewModel
 class BeerListViewModel @Inject constructor(
     private val getBeers: GetBeers,
-    private val uiMapper: UIMapper,
-    var state: Parcelable?,
-    private val dispatcher: CoroutineDispatcher
-) : ViewModel() {
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel(), CoroutineUtils by CoroutineManageContext(dispatcher) {
 
-    sealed class BeerListState {
-        class Error(val failure: Failure) : BeerListState()
-        class ShowItems(val beerList: List<UIBeer>) : BeerListState()
-        class IsLastPage(val isLastPage: Boolean) : BeerListState()
-        class IsLoading(val isLoading: Boolean) : BeerListState()
-    }
+    private val localState: MutableStateFlow<BeerListState> =
+        MutableStateFlow(BeerListState.Idle)
+    val state: StateFlow<BeerListState> = localState
 
-    sealed class NavigationEvent {
-        class ToDetail(val arg: UIBeer, val title: TextView) : NavigationEvent()
-    }
+    fun getBeers(fromStart: Boolean = true) {
 
-    private var _beersStateListData = MutableLiveData<BeerListState>()
-
-    val beersStateListData: LiveData<BeerListState>
-        get() = _beersStateListData
-
-
-//    Events
-
-    private val _navigation = MutableLiveData<Event<NavigationEvent>>()
-    val navigation: LiveData<Event<NavigationEvent>>
-        get() = _navigation
-
-
-    fun navigateTo(navEvent: NavigationEvent) {
-        _navigation.value = Event(navEvent)
-    }
-
-    fun getBeers(force: Boolean = false) {
-
-        _beersStateListData.value = BeerListState.IsLoading(true)
-
-        viewModelScope.launch(dispatcher) {
-            getBeers(GetBeers.Params(force))
-                .collect { value ->
-                    _beersStateListData.value =
-                        BeerListState.IsLoading(false)
-
-                    value.fold(::handleFailure) { listBeers ->
-                        _beersStateListData.value =
-                            BeerListState.IsLastPage(listBeers.isEmpty())
-                        also {
-                            _beersStateListData.value =
-                                BeerListState.ShowItems(uiMapper.convertBeersToUIBeers(listBeers))
-                        }
-                    }
+        viewModelScope.launch {
+            if (fromStart)
+                BeerListState.IsLoading.postOn(localState)
+            getBeers(Unit)
+                .map { listBeers ->
+                    BeerListState.ShowItems(listBeers.toUi()).postOn(localState)
+                }.mapLeft {
+                    handleFailure(it)
                 }
         }
+    }
 
+    fun loadMore() {
+        viewModelScope.launch {
+            when (val currentState = state.value) {
+                BeerListState.Idle,
+                is BeerListState.Error,
+                BeerListState.IsLoading,
+                is BeerListState.Paging -> Unit
+                is BeerListState.ShowItems -> {
+                    BeerListState.Paging(
+                        currentState.beerList
+                    ).postOn(localState)
+                    getBeers(false)
+                }
+            }
+        }
     }
 
     private suspend fun handleFailure(failure: Failure) {
-        _beersStateListData.value =
-            BeerListState.Error(
-                failure
-            )
+        BeerListState.Error(
+            failure
+        ).postOn(localState)
     }
-
 }
