6cf914ff26c9b24df44848d49c157849f68c2f7d
ivan <ivanfr89@gmail.com>
2022-08-29 12:00:45 +0200

    Fixed toolbar in MainActivity.kt

--- app/src/main/java/com/developer/ivan/beerapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/developer/ivan/beerapp/ui/main/MainActivity.kt
@@ -4,20 +4,23 @@ import BeerListScreen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.developer.ivan.beerapp.R
 import com.developer.ivan.beerapp.theme.BeerAppTheme
 import com.developer.ivan.beerapp.ui.main.screens.detail.BeerDetailScreen
 import com.developer.ivan.beerapp.androidbase.navigation.Feature
@@ -60,23 +63,23 @@ class MainActivity : AppCompatActivity() {
         val colorForSystemUi = MaterialTheme.colors.primaryVariant
 
         val currentRoute: String =
-            navController.currentBackStackEntryAsState().value?.destination?.route
-                ?: ""
+            navController.currentBackStackEntryAsState().value?.destination?.route.orEmpty()
 
         val showUpNavigation: Boolean =
             !NavItem.values().map { it.navCommand.route }.contains(currentRoute)
 
         Scaffold(
             topBar = {
-                if (showUpNavigation) {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = null,
-                            tint = MaterialTheme.colors.primaryVariant
-                        )
-                    }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(48.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                )
+                {
+                    Toolbar(showUpNavigation, navController)
                 }
+
             },
             content = { padding ->
                 Box(modifier = Modifier.padding(padding)) {
@@ -87,6 +90,27 @@ class MainActivity : AppCompatActivity() {
 
         SideEffect {
             systemUiController.setSystemBarsColor(color = colorForSystemUi)
+        }
+    }
+
+    @Composable
+    private fun Toolbar(
+        showUpNavigation: Boolean,
+        navController: NavHostController
+    ) {
+        if (showUpNavigation) {
+            IconButton(onClick = { navController.popBackStack() }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null,
+                    tint = MaterialTheme.colors.primaryVariant
+                )
+            }
+        } else {
+            Text(
+                modifier = Modifier.padding(start = 20.dp),
+                text = stringResource(id = R.string.beer_list)
+            )
         }
     }
 
