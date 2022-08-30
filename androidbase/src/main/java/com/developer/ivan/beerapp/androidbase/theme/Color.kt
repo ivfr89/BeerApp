c1020ef161caf2f213a2aec85d9e194e6f4563c1
ivan <ivanfr89@gmail.com>
2022-08-30 08:43:23 +0200

    Removed colors and added new ones on :androidbase

--- androidbase/src/main/java/com/developer/ivan/beerapp/androidbase/theme/Color.kt
+++ androidbase/src/main/java/com/developer/ivan/beerapp/androidbase/theme/Color.kt
@@ -2,6 +2,8 @@ package com.developer.ivan.beerapp.androidbase.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Green_50 = Color(0xFF98B487)
+val Red_60 = Color(0xFFF3A0BA)
 val Gray_50 = Color(0xFFB7BEC0)
 val Gray_80 = Color(0xFF343738)
 val White_10 = Color(0xFFF1F5F7)
